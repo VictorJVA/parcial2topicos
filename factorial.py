d24e7e177791d67b8fd515255cd5fc8b1b90afce
VictorJVA <vjvilladia@eafit.edu.co>
2024-11-06 07:51:24 -0500

    First commit

--- factorial.py
+++ factorial.py
@@ -4,16 +4,18 @@ app = Flask(__name__)
 
 def factorial(n):
     if n < 0:
-        return "Error: Tu numero no puede ser negativo"
+        return None
     elif n == 1 or n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
 
-@app.route('/factorial/<int(signed=True):n>', methods=['GET'])
+@app.route('/<int(signed=True):n>', methods=['GET'])
 def get_factorial(n):
-    factorial = factorial(n)
-    return jsonify(factorial)
-
+    n_factorial = factorial(n)
+    if n_factorial is None:
+        return jsonify(error="Error: El número no puede ser negativo"),
+    else:
+        return jsonify(number=n, factorial=n_factorial)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
