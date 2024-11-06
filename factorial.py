0e81f935835b4f5d489d5029f9737ba126c4b92e
VictorJVA <vjvilladia@eafit.edu.co>
2024-11-06 07:31:06 -0500

    First commit

---
+++ factorial.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def factorial(n):
+    if n < 0:
+        return "Numeros mayores o iguales a 0"
+    elif n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+@app.route('/factorial/<int(signed=True):n>', methods=['GET'])
+def get_factorial(n):
+    factorial = factorial(n)
+    return jsonify(factorial)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
